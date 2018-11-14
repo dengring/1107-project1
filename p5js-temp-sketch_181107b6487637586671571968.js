88a527a9465b69d00d3b6a8616420427f634cf0f
dengring <44824396+dengring@users.noreply.github.com>
2018-11-14 11:25:24 +0900

    Update p5js-temp-sketch_181107b6487637586671571968.js

--- p5js-temp-sketch_181107b6487637586671571968.js
+++ p5js-temp-sketch_181107b6487637586671571968.js
@@ -1,7 +1,37 @@
-function setup() {
+var cigarette;
+var candy;
+var gum;
+var bottle;
+var straw;
+var original;
 
+function setup() {
+  createCanvas(600,600);
+  cigarette=loadImage("cigarette.png");
+  candy=loadImage("candy.png");
+  gum=loadImage("gum.png");
+  bottle=loadImage("bottle.png");
+  straw=loadImage("straw.png");
+  original=loadImage("original.png");
 }
 
 function draw() {
-  ellipse(mouseX, mouseY, 50, 50);
+   if(mouseX>0 && mouseX<300 && mouseY>0 && mouseY<200) {
+     image(cigarette,0,0);
+   }
+   if(mouseX>300 && mouseX<600 && mouseY>0 && mouseY<200) {
+     image(candy,0,0);
+   }
+   if(mouseX>0 && mouseX<300 && mouseY>200 && mouseY<400) {
+     image(gum,0,0);
+   }
+   if(mouseX>300 && mouseX<600 && mouseY>200 && mouseY<400) {
+     image(bottle,0,0);
+   }
+   if(mouseX>0 && mouseX<300 && mouseY>400 && mouseY<600) {
+     image(straw,0,0);
+   }
+   if(mouseX>300 && mouseX<600 && mouseY>400 && mouseY<600) {
+     image(original,0,0);
+   }
 }
